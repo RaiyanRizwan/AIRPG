1a5b12408782420393a1ece0d4831fb9bf0191b6
RaiyanRizwan <raiyanbuilds@gmail.com>
2024-03-28 16:15:13 -0700

    prompt updates for dialogue

--- Log.py
+++ Log.py
@@ -1,13 +1,21 @@
+import os
 import csv
 import time
 
 class Log:
     
-    def __init__(self, start_time=0, filename='AIRPG_LOG.csv') -> None:
+    def __init__(self, start_time=0, foldername='logs') -> None:
         self.start_time = start_time
-        self.filename = filename
+        self.foldername = foldername
+        self.filename = self.setup_file()
         self.create_csv()
 
+    def setup_file(self):
+        os.makedirs(self.foldername, exist_ok=True)
+        existing_logs = [log for log in os.listdir(self.foldername) if log.startswith("AIRPG_LOG_") and log.endswith(".csv")]
+        log_number = len(existing_logs) + 1
+        return os.path.join(self.foldername, f'AIRPG_LOG_{log_number}.csv')
+    
     def create_csv(self):
         with open(self.filename, 'a', newline='') as file:
             writer = csv.writer(file)
--- NPC/NPC.py
+++ NPC/NPC.py
@@ -10,7 +10,8 @@ class NPC:
     """A generative NPC. Architectural ideas inspired from https://doi.org/10.1145/3586183.3606763."""
 
     WORLD_PREDICATE = 'Assume the medeival, fantastical world is reality. Never break the fourth wall. Magic is real.'
-    STYLE_PREDICATE = 'Speak in absolutes. Be concise and substantive.'
+    GENERAL_STYLE_PREDICATE = 'Speak in absolutes. Be concise and substantive.'
+    DIALOGUE_STYLE_PREDICATE = 'Be concise and substantive. Always respond in the format <character> : <facial expression / body language> <speech>.'
 
     def __init__(self, name: str, pronoun: str, age: int, traits: List[str], memory: Memory, LLM: GPTEndpoint, initial_description: str, time, reflection_buffer_length: int, log: Log) -> None:
         
@@ -38,15 +39,15 @@ class NPC:
 
         # core nature
         statements = self.memory.query(f'{self.name}\'s core characteristics.', 3, time) # statements relevant to core characteristics
-        msg_stream = [{"role": "user", "content": self.STYLE_PREDICATE + f'How would you describe {self.name}\'s core characteristics given the following statements?' + '\n'.join(statements)}]
+        msg_stream = [{"role": "user", "content": self.GENERAL_STYLE_PREDICATE + f'How would you describe {self.name}\'s core characteristics given the following statements?' + '\n'.join(statements)}]
         core_nature = self.LLM.complete(msg_stream)
         
         # life progress
         statements = self.memory.query(f'{self.name}\'s recent life progress.', 3, time) # statements relevant to life progress
-        msg_stream = [{"role": "user", "content": self.STYLE_PREDICATE + f'How would you describe {self.name}\'s feelings about {self.pronoun} current progress in life given the following statements?' + '\n'.join(statements)}]
+        msg_stream = [{"role": "user", "content": self.GENERAL_STYLE_PREDICATE + f'How would you describe {self.name}\'s feelings about {self.pronoun} current progress in life given the following statements?' + '\n'.join(statements)}]
         life_progress = self.LLM.complete(msg_stream)
 
-        self.character_summary = f'Name: {self.name} (age: {self.age}) \n Innate traits: {", ".join(self.traits)} \n {core_nature} {life_progress}'
+        self.character_summary = f'Name: {self.name} (age: {self.age}) \n Tone/Personality (ENSURE YOU TALK IN THIS MANNER, THIS IS OF UTMOST IMPORTANCE): {", ".join(self.traits)} \n {core_nature} {life_progress}'
         self.log.log(f'{self.name} generated a character summary: \n {self.character_summary}')
 
     def observe(self, observation: str, time) -> None:
@@ -64,7 +65,7 @@ class NPC:
         for question in questions: 
             relevant_memories = self.memory.query(question, 5, time)
             prompt = '\n'.join(relevant_memories) + '\n' + 'What one-sentence high-level insight can you infer from the above statements?'
-            insight = self.LLM.complete([{'role':'system', 'content': self.WORLD_PREDICATE + self.STYLE_PREDICATE},
+            insight = self.LLM.complete([{'role':'system', 'content': self.WORLD_PREDICATE + self.GENERAL_STYLE_PREDICATE},
                                           {'role':'user', 'content':prompt}])
             self.memory.record(insight, time)
             self.log.log(f'{self.name} had the following reflection: {insight} in response to the question {question}.')
@@ -80,11 +81,11 @@ class NPC:
         prompt = self.character_summary + '\n' + \
                 str(time) + '\n' + \
                 status + '\n' + \
-                context + \
+                f'You ONLY know whatever is in the following context. Be careful not to talk about ANYTHING you don\'t know about. Summary of relevant context from {self.name}\'s memory: ' + \
+                context + '\n' \
                 ' Here is the dialogue history:' + '\n' + \
                 '\n'.join(dialogue_history) + '\n' + \
-                f'{self.name}: <fill in>'
+                f'You are {self.name}. How would you respond?'
         self.log.log(f'Dialogue prompt: {prompt}')
-        return self.LLM.complete(message_stream=[{'role':'system', 'content': self.WORLD_PREDICATE + self.STYLE_PREDICATE}, 
-                                                 {'role':'system', 'content': f'ONLY RESPOND with dialogue for {self.name}: <fill in>.'},
+        return self.LLM.complete(message_stream=[{'role':'system', 'content': self.WORLD_PREDICATE + self.DIALOGUE_STYLE_PREDICATE}, 
                                                  {'role':'user', 'content':prompt}])
