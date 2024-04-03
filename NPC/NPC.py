9c36babe6e0545fc5ceaef95e628eb6147d8fe5e
RaiyanRizwan <raiyanbuilds@gmail.com>
2024-04-03 15:37:48 -0700

    NPC prompts class to make it easier to change and test and iterate on prompts

--- NPC/NPC.py
+++ NPC/NPC.py
@@ -1,4 +1,5 @@
 from NPC.Memory import Memory
+from NPC.Prompts import Prompts
 from GPTEndpoint import GPTEndpoint
 from Log import Log
 from typing import List
@@ -8,10 +9,6 @@ class NPC:
     # TODO: implement function to summarize and enter dialogue into memory when end of conversation occurs
 
     """A generative NPC. Architectural ideas inspired from https://doi.org/10.1145/3586183.3606763."""
-
-    WORLD_PREDICATE = 'Assume the medeival, fantastical world is reality. Never break the fourth wall. Magic is real.'
-    GENERAL_STYLE_PREDICATE = 'Speak in absolutes. Be concise and substantive.'
-    DIALOGUE_STYLE_PREDICATE = 'Be concise and substantive. Always respond in the format <character> : <facial expression / body language> <speech>.'
 
     def __init__(self, name: str, pronoun: str, age: int, traits: List[str], memory: Memory, LLM: GPTEndpoint, initial_description: str, time, reflection_buffer_length: int, log: Log) -> None:
         
@@ -25,6 +22,7 @@ class NPC:
         self.LLM = LLM
         self.log = log
         self.memory = memory
+        self.prompt = Prompts(self)
 
         # setup
         self.seed = initial_description # ; separated statements (entered into memory)
@@ -39,15 +37,15 @@ class NPC:
 
         # core nature
         statements = self.memory.query(f'{self.name}\'s core characteristics.', 3, time) # statements relevant to core characteristics
-        msg_stream = [{"role": "user", "content": self.GENERAL_STYLE_PREDICATE + f'How would you describe {self.name}\'s core characteristics given the following statements?' + '\n'.join(statements)}]
+        msg_stream = [{"role": "user", "content": self.prompt.core_characteristics(statements)}]
         core_nature = self.LLM.complete(msg_stream)
         
         # life progress
         statements = self.memory.query(f'{self.name}\'s recent life progress.', 3, time) # statements relevant to life progress
-        msg_stream = [{"role": "user", "content": self.GENERAL_STYLE_PREDICATE + f'How would you describe {self.name}\'s feelings about {self.pronoun} current progress in life given the following statements?' + '\n'.join(statements)}]
+        msg_stream = [{"role": "user", "content": self.prompt.life_progress(statements)}]
         life_progress = self.LLM.complete(msg_stream)
 
-        self.character_summary = f'Name: {self.name} (age: {self.age}) \n Tone/Personality (ENSURE YOU TALK IN THIS MANNER, THIS IS OF UTMOST IMPORTANCE): {", ".join(self.traits)} \n {core_nature} {life_progress}'
+        self.character_summary = self.prompt.character_summary(core_nature, life_progress)
         self.log.log(f'{self.name} generated a character summary: \n {self.character_summary}')
 
     def observe(self, observation: str, time) -> None:
@@ -57,16 +55,14 @@ class NPC:
     def reflect(self, time) -> None:
         """Condense last [self.reflection_buffer_length] memories into a high-level reflection (also entered into the memory stream)."""
         recent_memories = self.memory.memories_text[-self.reflection_buffer_length:]
-        msg_stream = [{'role':'system', 'content': self.WORLD_PREDICATE},
-                      {'role':'user', 'content':'Given only the information below, what are 3 most salient highlevel questions we can answer about the subjects in the statements? (example format: question \n question)'},
-                      {'role':'user', 'content':'\n'.join(recent_memories)}]
+        msg_stream = [{'role':'system', 'content': self.prompt.WORLD_PREDICATE},
+                      {'role':'user', 'content':self.prompt.salient_questions(recent_memories)}]
         questions = self.LLM.complete(msg_stream).split('\n')
         self.log.log(f'salient questions for {self.name}: {";".join(questions)}')
         for question in questions: 
             relevant_memories = self.memory.query(question, 5, time)
-            prompt = '\n'.join(relevant_memories) + '\n' + 'What one-sentence high-level insight can you infer from the above statements?'
-            insight = self.LLM.complete([{'role':'system', 'content': self.WORLD_PREDICATE + self.GENERAL_STYLE_PREDICATE},
-                                          {'role':'user', 'content':prompt}])
+            insight = self.LLM.complete([{'role':'system', 'content': self.prompt.WORLD_PREDICATE + self.prompt.GENERAL_STYLE_PREDICATE},
+                                          {'role':'user', 'content':self.prompt.insight(relevant_memories)}])
             self.memory.record(insight, time)
             self.log.log(f'{self.name} had the following reflection: {insight} in response to the question {question}.')
 
@@ -76,16 +72,7 @@ class NPC:
         status: what the NPC is currently doing in the world"""
         relevant_memories_receiver = self.memory.query(f'Who is {receiver_name}?', 1, time)
         relevant_memories_dialogue = self.memory.query(dialogue_history[-1], 3, time)
-        context = self.LLM.complete(message_stream=[{'role':'user', 'content':f'{self.name} is speaking to {receiver_name}. Briefly summarize the context given what {self.name} remembers: \n'},
-                                                    {'role':'user', 'content':"\n".join(relevant_memories_receiver + relevant_memories_dialogue)}])
-        prompt = self.character_summary + '\n' + \
-                str(time) + '\n' + \
-                status + '\n' + \
-                f'You ONLY know whatever is in the following context. Be careful not to talk about ANYTHING you don\'t know about. Summary of relevant context from {self.name}\'s memory: ' + \
-                context + '\n' \
-                ' Here is the dialogue history:' + '\n' + \
-                '\n'.join(dialogue_history) + '\n' + \
-                f'You are {self.name}. How would you respond?'
-        self.log.log(f'Dialogue prompt: {prompt}')
-        return self.LLM.complete(message_stream=[{'role':'system', 'content': self.WORLD_PREDICATE + self.DIALOGUE_STYLE_PREDICATE}, 
-                                                 {'role':'user', 'content':prompt}])
+        context = self.LLM.complete(message_stream=[{'role':'user', 'content':self.prompt.dialogue_context(receiver_name, relevant_memories_receiver, relevant_memories_dialogue)}])
+        self.log.log(f'Dialogue prompt: {self.prompt.dialogue(status, context, dialogue_history, time)}')
+        return self.LLM.complete(message_stream=[{'role':'system', 'content': self.prompt.WORLD_PREDICATE + self.prompt.DIALOGUE_STYLE_PREDICATE + self.prompt.DIALOGUE_STYLE_PREDICATE_EXAMPLE}, 
+                                                 {'role':'user', 'content':self.prompt.dialogue(status, context, dialogue_history, time)}])
---
+++ NPC/Prompts.py
@@ -0,0 +1,58 @@
+class Prompts:
+
+    def __init__(self, NPC) -> None:
+
+        self.NPC = NPC
+
+        # Establish the nature of the world the characters are in.
+        self.WORLD_PREDICATE = 'Assume the medeival, fantastical world is reality. Never break the fourth wall. Magic is real.'
+
+        # Establish response style (grammar, length, etc.) for reflections and character synthesis.
+        self.GENERAL_STYLE_PREDICATE = 'Speak in absolutes. Be concise and substantive.'
+
+        # Establish response style (grammar, length, etc.) for dialogue.
+        self.DIALOGUE_STYLE_PREDICATE = 'Be concise and substantive. Always respond in the format <character> : <speech>.'
+
+        # Few shot prompting examples.
+        self.DIALOGUE_STYLE_PREDICATE_EXAMPLE = "Some examples: " + " ".join([
+                                                                    'Alaric: For honor and glory!',
+                                                                    "Mira: Magic is not just power; it's wisdom.",
+                                                                    'Thorne: Shadows are my best allies.',
+                                                                    'Elara: My aim is true, as is my heart.',
+                                                                    'Drogan: Roarrr! Treasure? Mine!'
+                                                                ])
+    
+    def core_characteristics(self, statements):
+        """statements: statements relevant to core characteristics of NPC"""
+        return self.GENERAL_STYLE_PREDICATE + f'How would you describe {self.NPC.name}\'s core characteristics given the following statements?' + '\n'.join(statements)
+
+    def life_progress(self, statements):
+        """statements: statements relevant to how NPC is feeling about their life progress"""
+        return self.GENERAL_STYLE_PREDICATE + f'How would you describe {self.NPC.name}\'s feelings about {self.NPC.pronoun} current progress in life given the following statements?' + '\n'.join(statements)
+    
+    def character_summary(self, *args):
+        """return character summary"""
+        return f'Name: {self.NPC.name} (age: {self.NPC.age}) \n Tone/Personality (ENSURE YOU TALK IN THIS MANNER, THIS IS OF UTMOST IMPORTANCE): {", ".join(self.NPC.traits)} \n {args}'
+    
+    def salient_questions(self, recent_memories):
+        """generate salient questions prompt (for reflections)"""
+        return f'Given only the information below, what are 3 most salient highlevel questions we can answer about the subjects in the statements? \n' +  "\n".join(recent_memories)
+
+    def insight(self, relevant_memories):
+        """generate insight from memories (out: reflection)"""
+        return '\n'.join(relevant_memories) + '\n' + 'What one-sentence high-level insight can you infer from the above statements?'
+    
+    def dialogue_context(self, receiver_name, relevant_memories_receiver, relevant_memories_dialogue):
+        """summarize NPC's memories that are relevant to what's being spoken about"""
+        return f'{self.NPC.name} is speaking to {receiver_name}. Briefly summarize the context given what {self.NPC.name} remembers: \n' + '\n'.join(relevant_memories_receiver + relevant_memories_dialogue)
+    
+    def dialogue(self, status, context, dialogue_history, time):
+        """dialogue prompt"""
+        return self.NPC.character_summary + '\n' + \
+                str(time) + '\n' + \
+                status + '\n' + \
+                f'You ONLY know whatever is in the following context. Summary of relevant context from {self.NPC.name}\'s memory: ' + \
+                context + '\n' \
+                ' Here is the dialogue history:' + '\n' + \
+                '\n'.join(dialogue_history) + '\n' + \
+                f'You are {self.NPC.name}. How would you respond?'
